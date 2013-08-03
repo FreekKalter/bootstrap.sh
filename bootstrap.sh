2ad93ca72c5f3cbc8077a18c790436e261db3ef2
Freek Kalter <freek@kalteronline.org>
2013-08-03 18:59:53 +0200

    first

---
+++ bootstrap.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# $1 is project/dir name
+# $2 is optional language
+if [ -z $1 ]; then
+    exit
+fi
+DESTINATION=$1
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+LANG=$2
+
+mkdir $DESTINATION
+
+cd $DESTINATION
+# call git init
+git init
+cd $SCRIPT_DIR
+
+# Create gitignore
+echo "\
+.*.sw[a-z]
+tags\
+" > $DESTINATION/.gitignore
+
+# set up hooks for ctag generation
+# https://github.com/lyosha/ctags-go/blob/master/FAQ
+echo "\
+#!/bin/sh
+cd $DESTINATION
+ctags *
+find * -type d -prune -print | ctags -aR --file-scope=no -L" > $DESTINATION/.git/hooks/dirtags
+
+echo "\
+#!/bin/sh
+cd $GIT_DIR/..
+echo `pwd`
+find * -type d -exec dirtags {} \; " > $DESTINATION/.git/hooks/post-commit
+
+chmod +x $DESTINATION/.git/hooks/dirtags
+chmod +x $DESTINATION/.git/hooks/post-commit
+cp $DESTINATION/.git/hooks/post-commit $DESTINATION/.git/hooks/post-merge
+
+
+# Setup Pre-commit-hooks
+Pre-commit-hooks/install.sh $DESTINATION/.git/hooks
+cp gofmt-git-hook/fmt-check $DESTINATION/.git/hooks/pre-commit-gofmt
+
+# set up License file
+cp LICENSE $DESTINATION
+
+git add .
+
+git commit -m 'first'
+
+
