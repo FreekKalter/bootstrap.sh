61584b9b0acbf5391abf5ad9482b4291260c6a28
Freek Kalter <freek@kalteronline.org>
2013-08-03 22:04:50 +0200

    first

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,17 @@ if [ -z $1 ]; then
 fi
 DESTINATION=$1
 HOOK_DEST=$DESTINATION/.git/hooks
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+#SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $SCRIPT_DIR
 LANG=$2
 
 mkdir $DESTINATION
